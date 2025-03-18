f0bad8523cc72d824ecb541041fe49c72930966e
itohiro-soraile <116922465+itohiro-soraile@users.noreply.github.com>
2025-03-18 13:59:37 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -43,6 +43,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define HOLD_ON_OTHER_KEY_PRESS
 
-#define TAPPING_TERM 180
-
-#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM 100
+#define IGNORE_MOD_TAP_INTERRUPT
