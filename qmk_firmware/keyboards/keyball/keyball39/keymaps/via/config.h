21ca46558969134c087e3d735517c8bcc5cb5f97
itohiro-soraile <116922465+itohiro-soraile@users.noreply.github.com>
2025-03-18 14:28:09 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -39,8 +39,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 4
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
-#define AUTO_MOUSE_TIME 300
+#define AUTO_MOUSE_TIME 400
 
 #define HOLD_ON_OTHER_KEY_PRESS
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 190
+
+#define TAPPING_TERM_PER_KEY
+
+#define TAPPING_TERM_PER_KEY_OVERRIDE_ENABLE
