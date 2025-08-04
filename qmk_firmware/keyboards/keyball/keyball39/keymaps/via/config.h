4827583754dbfc90255921cf3c2e67fa159058e7
itohiro-soraile <116922465+itohiro-soraile@users.noreply.github.com>
2025-08-04 16:07:20 +0900

    update

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -37,8 +37,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 4
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
 
 #define AUTO_MOUSE_TIME 800
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 120
